6333e62266ef6e11f92076605ea65c1f1ed9d086
Jaime Gonzalez Aguilar <jaimegag@gmail.com>
2016-10-07 17:35:45 -0400

    First commit

---
+++ MVCAttendees/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAttendees
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
